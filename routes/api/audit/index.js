a9879cd5a8c08a6b968e2bf88bbd9c70db7cd51c
JakeV <jake.vandborg@gmail.com>
2019-11-03 09:06:03 +1030

    auditing bugfixes

--- routes/api/audit/index.js
+++ routes/api/audit/index.js
@@ -1,15 +1,16 @@
 const auth = require("../../../middleware/auth");
 const audit = require("../../../middleware/audit");
 const ApiAuditRouter = require("express").Router();
+const Event = require("../../../models/events");
 
 ApiAuditRouter.route("/")
-    // retrieves every action performed since events began being recorded, and exports them
-    // granted the requester has the relevant permissions
-    .get(auth.isLoggedIn, auth.auth, function(req, res) {
-        audit.saveEvent({user: req.user, action: "Auditing"});
-        Event.find({}, function(err, events) {
-            return res.render("audit", {events});
-        });
-    })
+  // retrieves every action performed since events began being recorded, and exports them
+  // granted the requester has the relevant permissions
+  .get(auth.isLoggedIn, auth.auth, function(req, res) {
+    audit.saveEvent({user: req.user, action: "Auditing"});
+    Event.find({}, function(err, events) {
+      return res.render("audit", {events});
+    });
+  });
 
 module.exports = ApiAuditRouter;
