8c890e8488497319081bbc18ce4f4bc698142449
Christian Piscioneri <a1669249@student.adelaide.edu.au>
2019-11-03 23:32:20 +1030

    Commented JS files

--- public/javascript/vote.js
+++ public/javascript/vote.js
@@ -1,3 +1,10 @@
+/*
+	Populates the vote.ejs UI with the ballot retrieved from the server.
+	Handles submitting the vote to the server.
+	Input can be seen when accessing /api/vote route.
+	Output on successful vote is the /api/sausage route/page.
+*/ 
+
 var aboveInput = new Map();
 var belowInput = new Map();
 var totalCandidates = 0;
@@ -12,35 +19,44 @@ function pageSetup(ballot){
 	var belowLine = document.getElementById('belowLine');
 	var submitBtn = document.getElementById('submitBtn');
 
+	// Set electorate name in the HTML
 	document.getElementById('electorateName').innerHTML = ballot.electorate;
 
 	totalParties = ballot.above.length;
 	ballot.above.forEach(function(party){
+		// Create a base for the party
 		let baseCol = document.createElement('div');
 		baseCol.classList.add('col');
 
+		// Create a div for the vote input
 		let inputCol = document.createElement('div');
 		inputCol.classList.add('col-4','mb-1');
 		baseCol.appendChild(inputCol);
 
+		// The vote input for a party
 		let voteInput = document.createElement('select');
 		voteInput.classList.add('custom-select');
 		voteInput.style.width = '60px';
 		voteInput.options.add(new Option('-',0));
+		// Set the input options
 		for (var i = 0; i < ballot.above.length; i++) {
 			voteInput.options.add(new Option(i+1,i+1));
 		}
 		inputCol.appendChild(voteInput);
 
+		// Div for party name
 		let partyNameCol = document.createElement('div');
 		partyNameCol.classList.add('col');
 		partyNameCol.innerHTML = party;
 		baseCol.appendChild(partyNameCol);
 
+		// Append to the HTML so it shows
 		aboveLine.appendChild(baseCol);
+		// Save the vote input for later so it can be read and submitted
 		aboveInput.set(party, voteInput);
 	});
 
+	// Count total candidates
 	for (var i = 0; i < ballot.below.length; i++){
 		totalCandidates += ballot.below[i].candidates.length;
 	}
@@ -72,6 +88,7 @@ function pageSetup(ballot){
 			voteInput.classList.add('custom-select');
 			voteInput.style.width = '60px';
 			voteInput.options.add(new Option('-',0));
+			// Set input options
 			for (var j = 0; j < totalCandidates; j++) {
 				voteInput.options.add(new Option(j+1,j+1));
 			}
@@ -83,15 +100,19 @@ function pageSetup(ballot){
 			canNameCol.innerHTML = party.candidates[i];
 			canCol.appendChild(canNameCol);
 
+			// Save input for later read and submit
 			belowInput.set(party.candidates[i], voteInput);
 		}
 
+		// Append to HTML so it appears
 		belowLine.appendChild(baseCol);
 	});
 
+	// Make the "submit" button submit the vote
 	submitBtn.addEventListener('click', submitVote);
 }
 
+// Gathers vote inputs and submits to the server
 function submitVote(){
 	if (
 	confirm("Once your vote is submitted it cannot be changed.\n Click 'OK' to submit your vote or 'Cancel' to revise.")
@@ -101,11 +122,15 @@ function submitVote(){
 			below: new Array(totalCandidates)
 		};
 
+		// Go through all the above the line inputs and save the selections
+		// as a "vote". Input tested to be a valid number.
 		aboveInput.forEach(function(input, key, map){
 			if (!isNaN(input.value) && input.value > 0 && input.value < totalParties)
 				vote.above[input.value-1] = key;
 		});
 
+		// Go through all the below the line inputs and save the selections
+		// as a "vote". Input tested to be a valid number.
 		belowInput.forEach(function(input, key, map){
 			if (!isNaN(input.value) && input.value > 0 && input.value < totalCandidates)
 				vote.below[input.value-1] = key;
@@ -113,7 +138,6 @@ function submitVote(){
 
 		// Send vote to server
 		$.post('/api/vote',vote).done(function(data) {
-			console.log("after post");
 			document.location = data.redirect;
 		});
 	}
