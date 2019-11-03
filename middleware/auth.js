0de33efee6505c203efadbd11078154bc4bc7add
Vincent <portellavincent@gmail.com>
2019-11-03 17:41:09 +1030

    create password

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,32 +1,39 @@
 const Role = require("../models/roles");
 
 module.exports = {
-    isLoggedIn: function(req, res, next){
-        if (req.isAuthenticated()) {
-            next();
-        } else {
-            res.redirect("/auth/logout");
-        }
-    },
-    ensureTotp: function(req, res, next){
-        if (
-            (req.session.secondFactor && req.session.method == "totp") ||
-            (!req.user.key && req.session.method == "plain")
-        ) {
-            next();
-        } else {
-            res.redirect("/auth/logout");
-        }
-    },
-    // checks for the relevant authorisation for a users actions
-    // if the desired action is not present in the permissions of their role, they are denied
-    // this function can be placed before any action to determine whether it should be completed
-    auth: function(req, res, next) {
-        Role.findOne({name: req.user.role}, function(err, role) {
-            let split = req.originalUrl.split("/");
-            if (role.permissions.includes(split[split.length-1])) {
-                next();
-            } else res.sendStatus(401);
-        });
-    }
+	isLoggedIn: function(req, res, next){
+		if (req.isAuthenticated()) {
+			next();
+		} else {
+			res.redirect("/auth/logout");
+		}
+	},
+	isNotLoggedIn: function(req, res, next) {
+		if (req.isUnauthenticated()) {
+			next();
+		} else {
+			res.redirect("/");
+		}
+	},
+	ensureTotp: function(req, res, next){
+		if (
+			(req.session.secondFactor && req.session.method == "totp") ||
+			(!req.user.key && req.session.method == "plain")
+		) {
+			next();
+		} else {
+			res.redirect("/auth/logout");
+		}
+	},
+	// checks for the relevant authorisation for a users actions
+	// if the desired action is not present in the permissions of their role, they are denied
+	// this function can be placed before any action to determine whether it should be completed
+	auth: function(req, res, next) {
+		Role.findOne({name: req.user.role}, function(err, role) {
+			let split = req.originalUrl.split("/");
+			if (role.permissions.includes(split[split.length-1])) {
+				next();
+			} else res.sendStatus(401);
+		});
+	}
 }
--- seed.js
+++ seed.js
@@ -8,10 +8,10 @@ var Ballot = require("./models/ballots");
 //Set up default mongoose connection
 //Format = mongodb+srv://<MongoDBUser>:<UserPassword>@<ClusterName>-cosb2.mongodb.net/test?retryWrites=true&w=majority
 var mongoDB =
-  "mongodb+srv://sseproject:sseproject@sseproject-cosb2.mongodb.net/myvote?retryWrites=true&w=majority";
+	"mongodb+srv://sseproject:sseproject@sseproject-cosb2.mongodb.net/myvote?retryWrites=true&w=majority";
 mongoose.connect(
-  mongoDB,
-  {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}
+	mongoDB,
+	{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}
 );
 
 //Get the default connection
@@ -21,136 +21,168 @@ var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 var ballots = [
-  {
-    electorate: "Adelaide",
-    above: ["Hemp Party", "Rain Party", "Other Hemp Party"],
-    below: [
-      {party: "Hemp Party", candidates: ["Michael Dab", "Michelle Doob"]},
-      {party: "Rain Party", candidates: ["Chief Tomahawk", "Chris Lake"]},
-      {party: "Other Hemp Party", candidates: ["Gregg Teeheytchsee", "Kate Trees"]}
-    ]
-  }
+	{
+		electorate: "Adelaide",
+		above: ["Hemp Party", "Rain Party", "Other Hemp Party"],
+		below: [
+			{party: "Hemp Party", candidates: ["Michael Dab", "Michelle Doob"]},
+			{party: "Rain Party", candidates: ["Chief Tomahawk", "Chris Lake"]},
+			{party: "Other Hemp Party", candidates: ["Gregg Teeheytchsee", "Kate Trees"]}
+		]
+	}
 ];
 
 var users = [
-  {
-    username: "jack",
-    password: "secret",
-    displayName: "Jack",
-    key: null,
-    role: "voter"
-  },
-  {
-    username: "jill",
-    password: "birthday",
-    displayName: "Jill",
-    key: null,
-    role: "hasVoted"
-  },
-  {
-    username: "bob",
-    password: "bobby300",
-    displayName: "Bob",
-    key: null,
-    role: "voter"
-  },
-  {
-    username: "toucanboy",
-    password: "qwertyuiop",
-    displayName: "Larry",
-    key: null,
-    role: "voter"
-  },
-  {
-    username: "delegate",
-    password: "secret",
-    displayName: "Delegate",
-    key: null,
-    role: "delegate"
-  },
-  {
-    username: "admin",
-    password: "admin",
-    displayName: "Admin",
-    key: null,
-    role: "admin"
-  }
+	{
+		// _id: mongoose.Types.ObjectId("1"),
+		username: "jack",
+		password: "secret",
+		displayName: "Jack",
+		key: null,
+		role: "voter",
+		passwordID: "sd7f8g5s78d",
+		active: true,
+		email: "a1705815@student.adelaide.edu.au",
+	},
+	{
+		// _id: mongoose.Types.ObjectId("2"),
+		username: "jill",
+		password: "birthday",
+		displayName: "Jill",
+		key: null,
+		role: "hasVoted",
+		passwordID: null,
+		active: false,
+		email: "hemp420@gmail.com",
+	},
+	{
+		// _id: mongoose.Types.ObjectId("3"),
+		username: "bob",
+		password: "bobby300",
+		displayName: "Bob",
+		key: null,
+		role: "voter",
+		passwordID: "df76gf7s89e7",
+		active: false,
+		email: "a1705958@student.adelaide.edu.au",
+	},
+	{
+		// _id: mongoose.Types.ObjectId("4"),
+		username: "toucanboy",
+		password: "qwertyuiop",
+		displayName: "Larry",
+		key: null,
+		role: "voter",
+		passwordID: "8s7dfg5hgd87",
+		active: false,
+		email: "a1704482@student.adelaide.edu.au",
+	},
+	{
+		username: "delegate",
+		password: "secret",
+		displayName: "Delegate",
+		key: null,
+		role: "delegate",
+		passwordID: null,
+		active: true,
+		email: "hempftw@gmail.com",
+	},
+	{
+		username: "admin",
+		password: "admin",
+		displayName: "Admin",
+		key: null,
+		role: "admin",
+		passwordID: null,
+		active: true,
+		email: "a1669249@student.adelaide.edu.au",
+	},
+	{
+		username: "test",
+		password: "sd768fg5ds87f6g5ds76fg",
+		displayName: "TESTER",
+		key: null,
+		role: "voter",
+		passwordID: "fd6gd78fgs7g",
+		active: false,
+		email: "a1621426@student.adelaide.edu.au",
+	}
 ];
 
 //the default roles
 var roles = [
-  {name: "voter", permissions: ["ballot"]},
-  {name: "hasVoted", permissions: []},
-  {name: "admin", permissions: ["createDelegate", "audit"]},
-  {name: "delegate", permissions: ["audit", "editBallot", "saveBallot", "ballot"]}
+	{name: "voter", permissions: ["ballot"]},
+	{name: "hasVoted", permissions: []},
+	{name: "admin", permissions: ["createDelegate", "audit", "account"]},
+	{name: "delegate", permissions: ["audit", "editBallot", "ballot", "countVotes"]}
 ];
 
 function createUser(details) {
-  return new Promise(function(resolve, reject) {
-    let user = new User(details);
-    user.save(function(err) {
-      if (err) {
-        reject(err);
-        console.log(err);
-        return;
-      }
-      resolve(user);
-      console.log(user);
-    });
-  });
+	return new Promise(function(resolve, reject) {
+		let user = new User(details);
+		user.save(function(err) {
+			if (err) {
+				reject(err);
+				console.log(err);
+				return;
+			}
+			resolve(user);
+			console.log(user);
+		});
+	});
 }
 
 function createBallot(details) {
-  return new Promise(function(resolve, reject) {
-    let ballot = new Ballot(details);
-    ballot.save(function(err) {
-      if (err) {
-        reject(err);
-        console.log(err);
-        return;
-      }
-      resolve(ballot);
-      console.log(ballot);
-    });
-  });
+	return new Promise(function(resolve, reject) {
+		let ballot = new Ballot(details);
+		ballot.save(function(err) {
+			if (err) {
+				reject(err);
+				console.log(err);
+				return;
+			}
+			resolve(ballot);
+			console.log(ballot);
+		});
+	});
 }
 
 function createRole(details) {
-  return new Promise(function(resolve, reject) {
-    let role = new Role(details);
-    role.save(function(err) {
-      if (err) {
-        reject(err);
-        console.log(err);
-        return;
-      }
-      resolve(role);
-      console.log(role);
-    });
-  });
+	return new Promise(function(resolve, reject) {
+		let role = new Role(details);
+		role.save(function(err) {
+			if (err) {
+				reject(err);
+				console.log(err);
+				return;
+			}
+			resolve(role);
+			console.log(role);
+		});
+	});
 }
 
 function seed(cb) {
-  let promises = [];
-  users.forEach(user => {
-    let promise = createUser(user);
-    promises.push(promise);
-  });
+	let promises = [];
+	users.forEach(user => {
+		let promise = createUser(user);
+		promises.push(promise);
+	});
 
-  roles.forEach(role => {
-    let promise = createRole(role);
-    promises.push(promise);
-  });
+	roles.forEach(role => {
+		let promise = createRole(role);
+		promises.push(promise);
+	});
 
-  ballots.forEach(ballot => {
-    let promise = createBallot(ballot);
-    promises.push(promise);
-  });
+	ballots.forEach(ballot => {
+		let promise = createBallot(ballot);
+		promises.push(promise);
+	});
 
-  Promise.all(promises).then(cb);
+	Promise.all(promises).then(cb);
 }
 
 seed(function() {
-  //Finished
-  mongoose.connection.close();
+	//Finished
+	mongoose.connection.close();
 });
